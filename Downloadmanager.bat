9c103394b15e4abd4319064a435916ca13e9bf53
xenix13 <105422151+xenix13@users.noreply.github.com>
2025-10-16 10:50:32 +0200

    Enhance Download Manager with new apps and updates
    Updated the Download Manager to include new applications and improved the update process.

--- Downloadmanager.bat
+++ Downloadmanager.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
-title Selection d applications a installer
+title Select applications to install
 color 1F
 
 :: ------------------ Auto-élévation ------------------
@@ -12,9 +12,15 @@ if %errorLevel% neq 0 (
 )
 
 :: ------------------------ Configuration ------------------------
+:update
+cls
+echo ================================
+echo   		Check Updates ...
+echo ================================
+echo.
 set "url=https://raw.githubusercontent.com/xenix13/Downloadmanager/refs/heads/main/Downloadmanager.bat"
 set "local=%~f0"
-set "localVersion=25.10.2b"
+set "localVersion=25.10.3"
 powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%local%.tmp'"
 set "versionURL=https://raw.githubusercontent.com/xenix13/Downloadmanager/refs/heads/main/Version.txt?t=%random%"
 set "tmpVersion=%temp%\version.tmp"
@@ -25,45 +31,45 @@ del "%tmpVersion%"
 
 
 :: ------------------------ Téléchargement ------------------------
-echo Check Updates...
+echo Check Version...
 
 :: Si téléchargement réussi, demande validation
 if exist "%local%.tmp" (
 	if not "!localVersion!"=="!remoteVersion!" (
 		echo Votre version : !localVersion!
-		set /p choice="Une mise a jour est disponible. La nouvelle version !remoteVersion! est disponible. Voulez-vous l installer ? (O/N) : "
-		if /I "!choice!"=="O" (
+		set /p choice="A new update !remoteVersion! is available. Do you want to upgrade ? (Y/N) : "
+		if /I "!choice!"=="O" | "Y" (
 			move /Y "%local%.tmp" "%local%"
-			echo Script mis a jour avec succes !
-			echo Relance du script...
+			echo Upgrade Succesfull !
+			echo Reload Script...
 			start "" "%local%"
 			exit /b
 		) else (
 			del "%local%.tmp"
-			echo Mise à jour ignoree, lancement du script...
+			echo Update ignored, Loading...
 			pause
 		)
 	) else (
-		echo Aucune mise a jour disponible.
+		echo None update is available.
 		del "%local%.tmp"
 		pause
 	)
 ) else (
-    echo Echec de la mise à jour, lancement du script...
+    echo Update failure. Loading script...
 	pause
 )
 
 :: ------------------ Initialisation des applications ------------------
 set "total=0"
 
-:: --- Navigateur ---
-set /a total+=1 & set "app[%total%]=[ ] Chrome"       & set "id[%total%]=Google.Chrome"                & set "cat[%total%]=Navigateur"
-set /a total+=1 & set "app[%total%]=[ ] Firefox"      & set "id[%total%]=Mozilla.Firefox"              & set "cat[%total%]=Navigateur"
-set /a total+=1 & set "app[%total%]=[ ] Opera"        & set "id[%total%]=Opera.Opera"                  & set "cat[%total%]=Navigateur"
-set /a total+=1 & set "app[%total%]=[ ] OperaGX"      & set "id[%total%]=Opera.OperaGX"                & set "cat[%total%]=Navigateur"
-set /a total+=1 & set "app[%total%]=[ ] Edge"         & set "id[%total%]=Microsoft.Edge"               & set "cat[%total%]=Navigateur"
-set /a total+=1 & set "app[%total%]=[ ] Vivaldi"      & set "id[%total%]=Vivaldi.Vivaldi"              & set "cat[%total%]=Navigateur"
-set /a total+=1 & set "app[%total%]=[ ] Yandex"      & set "id[%total%]=Yandex.Browser"              & set "cat[%total%]=Navigateur"
+:: --- Browsers ---
+set /a total+=1 & set "app[%total%]=[ ] Chrome"       & set "id[%total%]=Google.Chrome"                & set "cat[%total%]=Browsers"
+set /a total+=1 & set "app[%total%]=[ ] Firefox"      & set "id[%total%]=Mozilla.Firefox"              & set "cat[%total%]=Browsers"
+set /a total+=1 & set "app[%total%]=[ ] Opera"        & set "id[%total%]=Opera.Opera"                  & set "cat[%total%]=Browsers"
+set /a total+=1 & set "app[%total%]=[ ] OperaGX"      & set "id[%total%]=Opera.OperaGX"                & set "cat[%total%]=Browsers"
+set /a total+=1 & set "app[%total%]=[ ] Edge"         & set "id[%total%]=Microsoft.Edge"               & set "cat[%total%]=Browsers"
+set /a total+=1 & set "app[%total%]=[ ] Vivaldi"      & set "id[%total%]=Vivaldi.Vivaldi"              & set "cat[%total%]=Browsers"
+set /a total+=1 & set "app[%total%]=[ ] Yandex"      & set "id[%total%]=Yandex.Browser"              & set "cat[%total%]=Browsers"
 
 :: --- Communications ---
 set /a total+=1 & set "app[%total%]=[ ] Teams"        & set "id[%total%]=Microsoft.Teams"              & set "cat[%total%]=Communications"
@@ -90,33 +96,33 @@ set /a total+=1 & set "app[%total%]=[ ] OnlyOffice"   & set "id[%total%]=ONLYOFF
 set /a total+=1 & set "app[%total%]=[ ] Okular"   & set "id[%total%]=KDE.Okular"    & set "cat[%total%]=Documents"
 set /a total+=1 & set "app[%total%]=[ ] Adobe Creative Cloud"   & set "id[%total%]=Adobe.CreativeCloud"    & set "cat[%total%]=Documents"
 
-:: --- Jeux ---
-set /a total+=1 & set "app[%total%]=[ ] Steam"        & set "id[%total%]=Valve.Steam"                  & set "cat[%total%]=Jeux"
-set /a total+=1 & set "app[%total%]=[ ] Epic Games"   & set "id[%total%]=EpicGames.EpicGamesLauncher"  & set "cat[%total%]=Jeux"
-set /a total+=1 & set "app[%total%]=[ ] GOG Galaxy"   & set "id[%total%]=GOG.Galaxy"                   & set "cat[%total%]=Jeux"
-set /a total+=1 & set "app[%total%]=[ ] Ubisoft Connect" & set "id[%total%]=Ubisoft.Connect"           & set "cat[%total%]=Jeux"
-set /a total+=1 & set "app[%total%]=[ ] EA App"       & set "id[%total%]=ElectronicArts.EADesktop"     & set "cat[%total%]=Jeux"
-set /a total+=1 & set "app[%total%]=[ ] NVIDIA GEFORCE NOW" & set "id[%total%]=Nvidia.GeForceNow"       & set "cat[%total%]=Jeux"
-set /a total+=1 & set "app[%total%]=[ ] FACEIT Client" & set "id[%total%]=FACEITLTD.FACEITClient"      & set "cat[%total%]=Jeux"
-set /a total+=1 & set "app[%total%]=[ ] Bluestacks" & set "id[%total%]=Bluestack.Bluestacks"      & set "cat[%total%]=Jeux"
+:: --- Games ---
+set /a total+=1 & set "app[%total%]=[ ] Steam"        & set "id[%total%]=Valve.Steam"                  & set "cat[%total%]=Games"
+set /a total+=1 & set "app[%total%]=[ ] Epic Games"   & set "id[%total%]=EpicGames.EpicGamesLauncher"  & set "cat[%total%]=Games"
+set /a total+=1 & set "app[%total%]=[ ] GOG Galaxy"   & set "id[%total%]=GOG.Galaxy"                   & set "cat[%total%]=Games"
+set /a total+=1 & set "app[%total%]=[ ] Ubisoft Connect" & set "id[%total%]=Ubisoft.Connect"           & set "cat[%total%]=Games"
+set /a total+=1 & set "app[%total%]=[ ] EA App"       & set "id[%total%]=ElectronicArts.EADesktop"     & set "cat[%total%]=Games"
+set /a total+=1 & set "app[%total%]=[ ] NVIDIA GEFORCE NOW" & set "id[%total%]=Nvidia.GeForceNow"       & set "cat[%total%]=Games"
+set /a total+=1 & set "app[%total%]=[ ] FACEIT Client" & set "id[%total%]=FACEITLTD.FACEITClient"      & set "cat[%total%]=Games"
+set /a total+=1 & set "app[%total%]=[ ] Bluestacks" & set "id[%total%]=Bluestack.Bluestacks"      & set "cat[%total%]=Games"
 
 :: --- Compression ---
 set /a total+=1 & set "app[%total%]=[ ] 7-Zip"        & set "id[%total%]=7zip.7zip"                    & set "cat[%total%]=Compression"
 set /a total+=1 & set "app[%total%]=[ ] WinRAR"       & set "id[%total%]=RARLab.WinRAR"                & set "cat[%total%]=Compression"
 
-:: --- Outils ---
-set /a total+=1 & set "app[%total%]=[ ] TeamViewer"   & set "id[%total%]=TeamViewer.TeamViewer"        & set "cat[%total%]=Outils"
-set /a total+=1 & set "app[%total%]=[ ] AnyDesk"      & set "id[%total%]=AnyDesk.AnyDesk"              & set "cat[%total%]=Outils"
-set /a total+=1 & set "app[%total%]=[ ] Termius"      & set "id[%total%]=Termius.Termius"              & set "cat[%total%]=Outils"
-set /a total+=1 & set "app[%total%]=[ ] OpenVPN Connect" & set "id[%total%]=OpenVPNTechnologies.OpenVPNConnect" & set "cat[%total%]=Outils"
-set /a total+=1 & set "app[%total%]=[ ] Quickshare"   & set "id[%total%]=Quickshare"                   & set "cat[%total%]=Outils"
-set /a total+=1 & set "app[%total%]=[ ] Nextcloud Client" & set "id[%total%]=Nextcloud.NextcloudDesktop" & set "cat[%total%]=Outils"
-set /a total+=1 & set "app[%total%]=[ ] KeePassXC"    & set "id[%total%]=KeePassXCTeam.KeePassXC"      & set "cat[%total%]=Outils"
-set /a total+=1 & set "app[%total%]=[ ] Bitwarden"    & set "id[%total%]=Bitwarden.Bitwarden"          & set "cat[%total%]=Outils" 
-set /a total+=1 & set "app[%total%]=[ ] Nextcloud Password"    & set "id[%total%]=9NXVZ0ZP6D5Z"          & set "cat[%total%]=Outils"
-set /a total+=1 & set "app[%total%]=[ ] Rufus"    & set "id[%total%]=Rufus.Rufus"          & set "cat[%total%]=Outils"
-set /a total+=1 & set "app[%total%]=[ ] Itunes"    & set "id[%total%]=Apple.iTunes"          & set "cat[%total%]=Outils"
-set /a total+=1 & set "app[%total%]=[ ] Driver Store"    & set "id[%total%]=lostindark.DriverStoreExplorer"          & set "cat[%total%]=Outils"
+:: --- Tools ---
+set /a total+=1 & set "app[%total%]=[ ] TeamViewer"   & set "id[%total%]=TeamViewer.TeamViewer"        & set "cat[%total%]=Tools"
+set /a total+=1 & set "app[%total%]=[ ] AnyDesk"      & set "id[%total%]=AnyDesk.AnyDesk"              & set "cat[%total%]=Tools"
+set /a total+=1 & set "app[%total%]=[ ] Termius"      & set "id[%total%]=Termius.Termius"              & set "cat[%total%]=Tools"
+set /a total+=1 & set "app[%total%]=[ ] OpenVPN Connect" & set "id[%total%]=OpenVPNTechnologies.OpenVPNConnect" & set "cat[%total%]=Tools"
+set /a total+=1 & set "app[%total%]=[ ] Quickshare"   & set "id[%total%]=Quickshare"                   & set "cat[%total%]=Tools"
+set /a total+=1 & set "app[%total%]=[ ] Nextcloud Client" & set "id[%total%]=Nextcloud.NextcloudDesktop" & set "cat[%total%]=Tools"
+set /a total+=1 & set "app[%total%]=[ ] KeePassXC"    & set "id[%total%]=KeePassXCTeam.KeePassXC"      & set "cat[%total%]=Tools"
+set /a total+=1 & set "app[%total%]=[ ] Bitwarden"    & set "id[%total%]=Bitwarden.Bitwarden"          & set "cat[%total%]=Tools" 
+set /a total+=1 & set "app[%total%]=[ ] Nextcloud Password"    & set "id[%total%]=9NXVZ0ZP6D5Z"          & set "cat[%total%]=Tools"
+set /a total+=1 & set "app[%total%]=[ ] Rufus"    & set "id[%total%]=Rufus.Rufus"          & set "cat[%total%]=Tools"
+set /a total+=1 & set "app[%total%]=[ ] Itunes"    & set "id[%total%]=Apple.iTunes"          & set "cat[%total%]=Tools"
+set /a total+=1 & set "app[%total%]=[ ] Driver Store"    & set "id[%total%]=lostindark.DriverStoreExplorer"          & set "cat[%total%]=Tools"
 
 :: --- Developpement ---
 set /a total+=1 & set "app[%total%]=[ ] Visual Studio Code" & set "id[%total%]=Microsoft.VisualStudioCode" & set "cat[%total%]=Developpement"
@@ -124,8 +130,8 @@ set /a total+=1 & set "app[%total%]=[ ] WinSCP"       & set "id[%total%]=WinSCP.
 set /a total+=1 & set "app[%total%]=[ ] Notepad++"    & set "id[%total%]=Notepad++.Notepad++"          & set "cat[%total%]=Developpement"
 set /a total+=1 & set "app[%total%]=[ ] Git"          & set "id[%total%]=Git.Git"                      & set "cat[%total%]=Developpement"
 
-:: --- Personnalisation ---
-set /a total+=1 & set "app[%total%]=[ ] Lively Wallpaper"          & set "id[%total%]=rocksdanister.LivelyWallpaper"             & set "cat[%total%]=Personnalisation"
+:: --- Personnalization ---
+set /a total+=1 & set "app[%total%]=[ ] Lively Wallpaper"          & set "id[%total%]=rocksdanister.LivelyWallpaper"             & set "cat[%total%]=Personnalization"
 
 :: ------------------ Menu ------------------
 goto menu
@@ -134,7 +140,7 @@ goto menu
 color 1F
 cls
 echo ================================
-echo   Selectionne les applications
+echo   			Select Apps
 echo ================================
 echo.
 
@@ -174,16 +180,18 @@ for /L %%i in (1,1,%total%) do (
 if defined line echo.!line!
 
 echo.
-echo Tape le numero pour cocher/decocher.
-echo Tape I pour installer les applications selectionnees.
-echo Tape U pour desinstaller la selection.
-echo Tape Q pour quitter.
+echo Type the number to check/uncheck.
+echo Step I to install the selected applications.
+echo Step U to uninstall the selected applications.
+echo Step C to check update
+echo Step Q to quit.
 echo.
 set /p choix=Ton choix : 
 
 if /I "%choix%"=="Q" exit /b
 if /I "%choix%"=="I" goto install
 if /I "%choix%"=="U" goto uninstall
+if /I "%choix%"=="C" goto update
 
 :: Toggle
 for /L %%i in (1,1,%total%) do (
@@ -212,7 +220,7 @@ exit /b
 :install
 cls
 echo ================================
-echo   Installation en cours...
+echo   	Install in progress...
 echo ================================
 echo.
 for /L %%i in (1,1,%total%) do (
@@ -225,15 +233,15 @@ for /L %%i in (1,1,%total%) do (
         )
     )
 )
-echo Toutes les installations sont terminees.
-echo Appuyez sur une touche pour revenir au menu...
+echo All installations are complete.
+echo Press any key to return to the menu...
 pause >nul
 goto menu
 
 :uninstall
 cls
 echo ================================
-echo   Desinstallation en cours...
+echo   	Uninstall in progress...
 echo ================================
 echo.
 for /L %%i in (1,1,%total%) do (
@@ -246,10 +254,11 @@ for /L %%i in (1,1,%total%) do (
         )
     )
 )
-echo Toutes les desinstallations sont terminees.
-echo Appuyez sur une touche pour revenir au menu...
+echo All uninstallations are complete.
+echo Press any key to return to the menu...
 pause >nul
 goto menu
+
 
 
 
