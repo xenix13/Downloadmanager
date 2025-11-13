96712e8edb67bd76e640d2f23531a8dfbfadc8e8
xenix13 <105422151+xenix13@users.noreply.github.com>
2025-11-13 13:19:15 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- Downloadmanager.bat
+++ Downloadmanager.bat
@@ -144,7 +144,6 @@ set /a total+=1 & set "app[%total%]=[ ] Onedrive" & set "id[%total%]=Microsoft.O
 set /a total+=1 & set "app[%total%]=[ ] VirtualBox" & set "id[%total%]=Oracle.VirtualBox"      & set "cat[%total%]=Tools"
 set /a total+=1 & set "app[%total%]=[ ] Sweet Home 3D" & set "id[%total%]=eTeks.SweetHome3D"     & set "cat[%total%]=Tools"
 set /a total+=1 & set "app[%total%]=[ ] Copilot" & set "id[%total%]=9NHT9RB2F4HD"     & set "cat[%total%]=Tools"
-set /a total+=1 & set "app[%total%]=[ ] NVIDIA App"  & set "idweb[%total%]=powershell -Command "Invoke-WebRequest -Uri 'https://uk.download.nvidia.com/nvapp/client/11.0.5.420/NVIDIA_app_v11.0.5.420.exe' -OutFile \"$env:USERPROFILE\Desktop\NVIDIA_App.exe""  & set "cat[%total%]=Tools"
 
 :: --- Admin & Dev---
 set /a total+=1 & set "app[%total%]=[ ] Visual Studio Code" & set "id[%total%]=Microsoft.VisualStudioCode" & set "cat[%total%]=Admin & Dev"
@@ -310,6 +309,7 @@ echo All uninstallations are complete.
 echo Press any key to return to the menu...
 pause >nul
 goto menu
+
 
 
 
